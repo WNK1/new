5e3ea07196e48f36b51432acfa041c741fc0a7f3
Nik <117580411+WNK1@users.noreply.github.com>
2024-09-04 19:51:08 +0500

    Add files via upload

---
+++ r.js
@@ -0,0 +1,16 @@
+    console.log('я')
+    console.log('стану')
+    console.log('программистом')
+
+    let UserEyeColor=('brown')
+    console.log(UserEyeColor)
+
+    const block = {
+        UserName: 'Nik',
+        Name : 'Nik'
+    }
+    console.log(block.Name)
+
+    
+
+
