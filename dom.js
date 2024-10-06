fb79830b0d92d99d472c6a97347e1927ceb81818
Nik <117580411+WNK1@users.noreply.github.com>
2024-10-06 19:54:26 +0500

    Add files via upload

--- dom.js
+++ dom.js
@@ -168,9 +168,16 @@ const sledComments = document.querySelector('.h1')
 const sled = sledComments.nextSibling;
 console.log(sled)//мы получаем коллекцию с данными объекта 
 //а что бы нам как в данном случаеп просто получить текст комментария пропишем следущее 
-console.log(sled.data)
-
+console.log(sled.data) 
+//???????????? почему нужно что бы коментраий вставал на одну строчку с закрытием придыдушего тега а не начинался с новой строки ????
 /////////////////////////////// ошибка , СПРОСИТЬ!!!!!!!!!!!!!!
+
+
+
+
+
+
+
 //так же можем изменить наш текстовый узел / комментарий и тд
 sled.data = 'не коммент';
 console.log(sled.data)  ////////////////////////!!!!!!!!!!!!!111 ПОЧЕМУ ПОЯВЛЯЕТСЯ НА СТРАНИЦЕ КАК ВИДИМЫЙ ЭЛЕМЕНТ 
@@ -183,3 +190,207 @@ console.log(news);
 //вписываем данный в новосозданный элемент 
 news.innerHTML = `Это Новосозданный элемент с помощью JS кода и тегов .createElement а так же заполнен с помощью тега .innerHTML`;
 console.log(news);
+//Если нужно полноценное создание нового текстового узла то делаем это с помощью метода createTextNode
+const newYzel = document.createTextNode('Новый узел');
+console.log(newYzel);
+
+//на данный момент наши данные не вставлениы в html документ , для того что бы они заняли свои места там сущестуют методы 
+//перед объектом 
+//sledComments.before(news);
+
+// вставить наш текст объкт и тд после селектора который мы искали , мы используем after
+//sledComments.after(news)
+
+//если мы хотим вставить содеримое во внутрь селектора и что бы он был перед определенным текстом например то исп метод prepend
+//sledComments.prepend(news)
+//если хотим появления внутри , но уже после оределенного текста то метод append
+sledComments.append(news);
+
+
+//метод вставки наглядный пример 
+/*
+                    --after 
+<ul class="dfs">
+
+            --prepend
+    <li></li>
+    <li></li>
+    <li></li>
+            --append
+
+</ul>
+                    --before
+*/
+
+//так же с помощью этих методов можно делать всавки сразу нескольких объектов просто перечисляя их через запятую 
+// .append(newElemen,"hi")
+
+
+
+
+//Метод вставки текста  insertAdjacentElement()
+const textElement = document.querySelector('.h1')
+//теперь вставляем текст который мы хотим что бы появился 
+//textElement.insertAdjacentElement(
+    'afterend', //метод или же свойтво куда именно вставляется элемент
+    `<p>Новая вставка с помощью <span>insertAdjacentElement()</span></p>`
+
+//)
+
+/* 
+мотоды вставок и куда элементы поместятся 
+
+
+beforebegin-вставка теста непосредственно перед h1
+afterbegin-после h1
+beforeend-вставка во внутрь в конец textElement
+afterend - в конец textElement
+
+
+    --beforebegin
+<div> 
+
+            --afterbegin
+ <p> здесь текст</p>
+            --beforeend
+
+</div>
+    --afterend
+*/
+
+
+//ПЕРЕНОС ЭЛЕМЕНТА 
+/* мы можем вставлять не только новый узлы 
+но и переносить старые так как они автоматически удаляются со старых мест 
+*/
+
+//получаем по традиции объект 
+const perenos = document.querySelector('.h1') //класс или что либо другое где находиться то что нужно перенести 
+//получаем объект 
+const h3 = document.querySelector('h3');//то что нам нужно перенести 
+//выполеняем сам перенос 
+perenos.prepend('h3');
+
+
+//если нам нужно не переноситть объект а например сделать его клон то нужно воспользоваться методом cloneNode()
+const clone= document.querySelector('.h1');
+const newClone = clone.cloneNode(true); //данное клонирование происходит без элементов - теста и другой информации / мы просто копируем клас константу и тд без ее содержиого 
+//а если же енам нужно полностью клонировать элемент со всем его содержимым то нужно исп глубокое клонировани 
+//просто внутри скобок метода прописать true 
+const kuda = document.querySelector('.main__animation');
+kuda.prepend(newClone);
+
+
+//если нужно удалитьобъект то просто использем метод .remove()
+//определяем объект 
+const removing = document.querySelector('.h1')
+removing.remove();
+
+const removings = document.querySelector('.h1')
+removings.remove();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Управление классами и стилями классов 
+//свойства className & classList 
+
+//className 
+//получаем объект с неким классом 
+const polClass = document.querySelector('.animation__first')
+//получаем именна классов 
+const namesClass = polClass.className;
+console.log(namesClass);
+
+//далее перезаписываем имя класса 
+//polClass.className= 'red';
+//НО ЭТОТ СПОСОБ ПЕРЕЗАПИШЕТ ВСЕ КЛАССЫ И ИЗМЕНИТ СТРУКТУРУ А ЭТО НАМ НЕ НУЖНО ТАК ЧТО ...
+
+//ПРИМЕНЯЕМ classList -это специальный объект сос твоими методами и свойтвами / для добавления или удаления одного класса 
+//1 получаем элемент 
+const prh = document.querySelector('.animation__first');
+
+//методы 
+/*
+//добавление класса 
+prh.classList.add('что то')
+
+//удаление класса 
+prh.classList.remove('что то')
+
+//добавить класс если его нет , если же он есть удалить его 
+prh.classList.toggle('что то')
+
+//проверка наличия класса - возврат true/false
+prh.classList.contains('что то ')
+*/
+//практика 
+const newIzmenenie = document.querySelector('.animation__first') //получение элемента у которого будет что то деалть с классами 
+newIzmenenie.classList.add('new_classes')
+
+if (newIzmenenie.classList.contains('new_classes')){  //функцией проверяем и выводим есть ли у нас такой класс 
+    console.log(`Добавлен новый класс`)
+} else {
+    console.log(`ошибка , класс не найден`)
+};
+
+
+/*const mi = document.querySelector('.main__animation')
+//classList это перебераемое значение так что мы можем перебрать все классы того или иного объекта 
+for ( const vseClassi of mi ){
+    console.log(vseClassi);
+}; //ошибка its not iterable 
+
+*/
+
+//так же через js мы можем управлять стилями элементов 
+//опять же определяем объект 
+const styles = document.querySelector('.animation__first')
+//обращаемся к константе и прописываем свойсто .style а дальше то что хотим изменить как css
+styles.style.color = "whitesmoke";
+styles.style.fontSize = "16px"; //если свойство состоит из 2х и более слов то стоит его прописыватьв lowerCamelCase как в данном примере 
+
+// если мы хотим узнать какое значение принимает то или инное свойство нам достаточно к нему обратиться 
+console.log(styles.style.fontSize);
+
+//если нам нужно сбросить то или инное свойсто по какой то причине то ему просто надо присвоить пустую строку 
+styles.style.marginBottom = ""; //сброс силей данного свойства 
+
+
+
+
+
+
+
+//получение стилей / вычисление с помощью метода getComputedStyle(element.css - свойсво)
+//применимо когда свойсто записано только в css(всегда) а в html его нет
+
+//1 определение переменной 
+const computed= document.querySelector('.navigation__ul')
+//после чего выводим в консоль свойство которое хотим узнать вместе с методом
+const finishedComputed = getComputedStyle(computed);
+console.log(finishedComputed.display);
+
+//точно так же можно получать и стиле псевлоэлемента объекта например before и тд
+const finishedComputedS = getComputedStyle(computed,'::before'); //пример такого получения 
+console.log(finishedComputedS.color);
+
+
+//если нам нужно скрыть свойство - элемент то мы можем использовать метод .hidden 
+//1 определяем элемент 
+const hiddenTitle2= document.querySelector('.header__container')
+console.log(h3)
+h3.hidden = true;//присваеваем элементу значение true и тогда элемент будет скрыт со сраницы 
+console.log(h3.hidden)
+
